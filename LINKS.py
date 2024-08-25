b42607604a8add3cb43ca6da5467cd098c593a8d
QAFed <143259597+QAFed@users.noreply.github.com>
2024-08-25 19:39:18 +0300

    test 1 refactor + test 2

--- LINKS.py
+++ LINKS.py
@@ -6,6 +6,10 @@ class RegisterXpath:
     name = '/html/body/div/div/main/div/form/fieldset[1]/div/div/input'
     email = '/html/body/div/div/main/div/form/fieldset[2]/div/div/input'
     password = '/html/body/div/div/main/div/form/fieldset[3]/div/div/input'
+    button_register = '/html/body/div/div/main/div/form/button[text()="Зарегистрироваться"]'
+
+class RegisterCSS:
+    pass_field_alert = '#root > div > main > div > form > fieldset:nth-child(3) > div > p'
 
 class LoginXpath:
-    button_login = '/html/body/div/div/main/div/form/button'
+    button_login = '//button[text()="Войти"]'
--- tests/test_register_page.py
+++ tests/test_register_page.py
@@ -1,6 +1,7 @@
 import time
 from LINKS import URLpage
 from LINKS import RegisterXpath
+from LINKS import RegisterCSS
 from LINKS import LoginXpath
 from selenium.webdriver.common.by import By
 from datetime import datetime
@@ -13,18 +14,27 @@ def test_register_form_if_name_email_pass_correct_go_auth_page(driver):
     name = f"name{gen_id}"
     email = f"FedorIdolenkov_{gen_id}@ya.ya"
     password = f"psswrd{gen_id}"
-    time.sleep(5)
     driver.get(URLpage.REGISTER)
     WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located((By.XPATH, RegisterXpath.name)))
     driver.find_element(By.XPATH, RegisterXpath.name).send_keys(name)
     driver.find_element(By.XPATH, RegisterXpath.email).send_keys(email)
     driver.find_element(By.XPATH, RegisterXpath.password).send_keys(password)
-
-    time.sleep(5)
-    driver.find_element(By.XPATH, '/html/body/div/div/main/div/form/button').click()
-    time.sleep(5)
-    WebDriverWait(driver, 10).until(
-        expected_conditions.visibility_of_element_located(
-            (By.XPATH, LoginXpath.button_login))
-    )
+    driver.find_element(By.XPATH, RegisterXpath.button_register).click()
+    WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located((By.XPATH, LoginXpath.button_login)))
     assert driver.current_url == URLpage.LOGIN
+
+
+def test_alert_text_if_pass_less_than_6_char(driver):
+    gen_id = datetime.now().strftime("%Y%m%d%H%M%S")
+    name = f"name{gen_id}"
+    email = f"FedorIdolenkov_{gen_id}@ya.ya"
+    password = '12345'
+    driver.get(URLpage.REGISTER)
+    WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located((By.XPATH, RegisterXpath.name)))
+    driver.find_element(By.XPATH, RegisterXpath.name).send_keys(name)
+    driver.find_element(By.XPATH, RegisterXpath.email).send_keys(email)
+    driver.find_element(By.XPATH, RegisterXpath.password).send_keys(password)
+    driver.find_element(By.XPATH, RegisterXpath.button_register).click()
+    WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located((By.CSS_SELECTOR, RegisterCSS.pass_field_alert)))
+    alert_text = driver.find_element(By.CSS_SELECTOR, RegisterCSS.pass_field_alert).text
+    assert alert_text == 'Некорректный пароль'
