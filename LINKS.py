d74e849c8eadbbdc2cd47f59d8d6cb1aec2660c4
QAFed <143259597+QAFed@users.noreply.github.com>
2024-08-25 18:52:06 +0300

    test 1 ok

--- LINKS.py
+++ LINKS.py
@@ -1,1 +1,11 @@
-URL_REGISTER_PAGE = 'https://stellarburgers.nomoreparties.site/register'
+class URLpage:
+    REGISTER = 'https://stellarburgers.nomoreparties.site/register'
+    LOGIN = 'https://stellarburgers.nomoreparties.site/login'
+
+class RegisterXpath:
+    name = '/html/body/div/div/main/div/form/fieldset[1]/div/div/input'
+    email = '/html/body/div/div/main/div/form/fieldset[2]/div/div/input'
+    password = '/html/body/div/div/main/div/form/fieldset[3]/div/div/input'
+
+class LoginXpath:
+    button_login = '/html/body/div/div/main/div/form/button'
--- tests/test_register_page.py
+++ tests/test_register_page.py
@@ -1,5 +1,7 @@
 import time
-from LINKS import URL_REGISTER_PAGE
+from LINKS import URLpage
+from LINKS import RegisterXpath
+from LINKS import LoginXpath
 from selenium.webdriver.common.by import By
 from datetime import datetime
 from selenium.webdriver.support.wait import WebDriverWait
@@ -7,12 +9,22 @@ from selenium.webdriver.support import expected_conditions
 
 
 def test_register_form_if_name_email_pass_correct_go_auth_page(driver):
-    name = f"Имя{datetime.now().strftime("%Y%m%d_%H%M%S")}"
-    time.sleep(10)
-    driver.get(URL_REGISTER_PAGE)
-    WebDriverWait(driver, 10).until(
-        expected_conditions.visibility_of_element_located((By.XPATH, '/html/body/div/div/main/div/form/button'))
-    )
+    gen_id = datetime.now().strftime("%Y%m%d%H%M%S")
+    name = f"name{gen_id}"
+    email = f"FedorIdolenkov_{gen_id}@ya.ya"
+    password = f"psswrd{gen_id}"
+    time.sleep(5)
+    driver.get(URLpage.REGISTER)
+    WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located((By.XPATH, RegisterXpath.name)))
+    driver.find_element(By.XPATH, RegisterXpath.name).send_keys(name)
+    driver.find_element(By.XPATH, RegisterXpath.email).send_keys(email)
+    driver.find_element(By.XPATH, RegisterXpath.password).send_keys(password)
 
-    driver.find_element(By.CLASS_NAME, 'text input__textfield text_type_main-default').send_keys(name)
-    time.sleep(10)
+    time.sleep(5)
+    driver.find_element(By.XPATH, '/html/body/div/div/main/div/form/button').click()
+    time.sleep(5)
+    WebDriverWait(driver, 10).until(
+        expected_conditions.visibility_of_element_located(
+            (By.XPATH, LoginXpath.button_login))
+    )
+    assert driver.current_url == URLpage.LOGIN
