98d4ab29035cfbe23f05b91459cd51a3972fbaa9
Fedor <idolenkov@gmail.com>
2024-08-30 16:20:32 +0300

    Можно лучше: урлы стоит конкатенировать с базовым, так будет проще поддерживать несколько стендов

--- links.py
+++ links.py
@@ -1,9 +1,9 @@
 class URLpage:
-    REGISTER = 'https://stellarburgers.nomoreparties.site/register'
-    LOGIN = 'https://stellarburgers.nomoreparties.site/login'
     START = 'https://stellarburgers.nomoreparties.site/'
-    FORGOT = 'https://stellarburgers.nomoreparties.site/forgot-password'
-    PERSON = 'https://stellarburgers.nomoreparties.site/account/profile'
+    REGISTER = START+'register'
+    LOGIN = START+'login'
+    FORGOT = START+'forgot-password'
+    PERSON = START+'account/profile'
 
 
 
