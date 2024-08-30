bcd114ea79870bc604866b4f2d880b11a212a2b0
Fedor <idolenkov@gmail.com>
2024-08-30 17:44:30 +0300

    Нужно исправить: здесь и далее: использование порядкового номера элемента в dom является весьма нестабильным способом поиска

--- locators.py
+++ locators.py
@@ -1,39 +1,36 @@
 class RegisterXpath:
-    name = '/html/body/div/div/main/div/form/fieldset[1]/div/div/input'
-    email = '/html/body/div/div/main/div/form/fieldset[2]/div/div/input'
-    password = '/html/body/div/div/main/div/form/fieldset[3]/div/div/input'
-    button_register = '/html/body/div/div/main/div/form/button[text()="Зарегистрироваться"]'
-    link_to_login = '/html/body/div/div/main/div/div/p/a[text()="Войти"]'
-
-
-class RegisterCSS:
-    pass_field_alert = '#root > div > main > div > form > fieldset:nth-child(3) > div > p'
+    name = '//label[text()="Имя"]'
+    email = '//label[text()="Email"]'
+    password = '//label[text()="Пароль"]'
+    button_register = '//button[text()="Зарегистрироваться"]'
+    link_to_login = '//a[@href="/login"]'
+    pass_field_alert = '//p[contains(@class, "error text")]'
 
 
 class LoginXpath:
     button_login = '//button[text()="Войти"]'
-    email = '/html/body/div/div/main/div/form/fieldset[1]/div/div/input'
-    password = '/html/body/div/div/main/div/form/fieldset[2]/div/div/input'
+    email = '//label[text()="Email"]'
+    password = '//label[text()="Пароль"]'
 
 
 class StartXpath:
-    button_login = '/html/body/div/div/main/section[2]/div/button[text()="Войти в аккаунт"]'
-    button_personal_account = '/html/body/div/div/header/nav/a/p[text()="Личный Кабинет"]'
+    button_login = '//button[text()="Войти в аккаунт"]'
+    button_personal_account = '//p[text()="Личный Кабинет"]'
     button_order = '//button[text()="Оформить заказ"]'
-    button_bulki = '/html/body/div/div/main/section[1]/div[1]/div[1]'
-    button_bulki_activated = '/html/body/div/div/main/section[1]/div[1]/div[1][contains(@class,"current")]'
-    button_sousi = '/html/body/div/div/main/section[1]/div[1]/div[2]'
-    button_sousi_activated = '/html/body/div/div/main/section[1]/div[1]/div[2][contains(@class,"current")]'
-    button_nachinki = '/html/body/div/div/main/section[1]/div[1]/div[3]'
-    button_nachinki_activated = '/html/body/div/div/main/section[1]/div[1]/div[3][contains(@class,"current")]'
+    button_bulki = '//span[text()="Булки"]'
+    button_bulki_activated = '//span[text()="Булки"]/parent::div'
+    button_sousi = '//span[text()="Соусы"]'
+    button_sousi_activated = '//span[text()="Соусы"]/parent::div'
+    button_nachinki = '//span[text()="Начинки"]'
+    button_nachinki_activated = '//span[text()="Начинки"]/parent::div'
 
 
 class ForgotXpath:
-    link_to_login = '/html/body/div/div/main/div/div/p/a[text()="Войти"]'
+    link_to_login = '//a[text()="Войти"]'
 
 
 class PersonXpath:
-    login = '/html/body/div/div/main/div/div/div/ul/li[2]/div/div/input'
-    link_to_constructor = '/html/body/div/div/header/nav/ul/li[1]/a/p[text()="Конструктор"]'
-    logo_stellar = '/html/body/div/div/header/nav/div/a'
-    button_exit = '/html/body/div/div/main/div/nav/ul/li[3]/button'
+    login = '//label[text()="Логин"]'
+    link_to_constructor = '//p[text()="Конструктор"]'
+    logo_stellar = '//div[contains(@class, "logo")]'
+    button_exit = '//button[text()="Выход"]'
